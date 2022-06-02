cb5d265f251873f7c77862438d5d3db16f2d9400
Deepak Marasandra <deepak@frollo.us>
2022-06-02 18:02:30 +1000

    Add keep annotation

--- src/main/kotlin/us/frollo/frollosdk/model/coredata/aggregation/accounts/PropertyZoning.kt
+++ src/main/kotlin/us/frollo/frollosdk/model/coredata/aggregation/accounts/PropertyZoning.kt
@@ -16,10 +16,12 @@
 
 package us.frollo.frollosdk.model.coredata.aggregation.accounts
 
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 import us.frollo.frollosdk.extensions.serializedName
 
 /** Property Zoning */
+@Keep
 enum class PropertyZoning {
 
     /** Residential */
