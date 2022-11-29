34c5a27e7ddfbec15413af92296a5fdd0d4f4e13
Vindhya Rajagopal <vindhya.rajagopal@frollo.us>
2022-11-29 11:48:32 +1100

    Add cdr_config table

--- src/main/kotlin/us/frollo/frollosdk/database/SDKDatabase.kt
+++ src/main/kotlin/us/frollo/frollosdk/database/SDKDatabase.kt
@@ -857,7 +857,7 @@ abstract class SDKDatabase : RoomDatabase() {
                 // and "sharing_use_description" in the converter method stringToListOfSharingDuration.
                 // It should be okay as upon migration the cdr config data is fetched again from the host
                 // and the cache will be updated.
-                database.execSQL("") // TODO Create table
+                database.execSQL("CREATE TABLE IF NOT EXISTS `cdr_configuration` (`config_id` INTEGER NOT NULL, `support_email` TEXT NOT NULL, `sharing_durations` TEXT NOT NULL, `permissions` TEXT, `additional_permissions` TEXT, `external_id` TEXT NOT NULL, `display_name` TEXT NOT NULL, `cdr_policy_url` TEXT NOT NULL, `model` TEXT NOT NULL, `related_parties` TEXT NOT NULL, `initial_sync_window_weeks` INTEGER, `software_id` TEXT, `software_name` TEXT, `image_url` TEXT, `summary` TEXT, `description` TEXT, PRIMARY KEY(`config_id`))")
                 database.execSQL("CREATE INDEX IF NOT EXISTS `index_cdr_configuration_config_id` ON `cdr_configuration` (`config_id`)")
                 // END - Alter table - cdr_configuration - Drop column sharing_use_duration
             }
